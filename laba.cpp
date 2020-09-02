17037d278d338141256ffc17f90ea2a8928689c2
John MG <johnmg@MacBook-Pro-John.local>
2020-09-02 22:15:07 +0300

    first proga

---
+++ laba.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int sum = 0;
+    setlocale(0, "");
+    for (i = 1; i <= 1000; i++)
+    {
+        sum = sum + i;
+    }
+    cout << "Сумма чисел =" << sum << endl;
+    return 0;
+}
