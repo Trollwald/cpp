53a01abc7d6c6f462e2021de165010dd1b084af1
John MG <johnmg@MacBook-Pro-John.local>
2020-09-03 23:30:17 +0300

    itest

--- laba.cpp
+++ laba.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <math.h>
-using namespace std;
 int main()
 {
-    double x, y, z, a, b, s;
-    cout << "Vvedi x:";
-    cin >> x;
-    cout << "Vvedi y:";
-    cin >> y;
-    cout << "Vvedi z:";
-    cin >> z;
-    a = pow(y + pow(x - 1, 1 / 3.), 1 / 4.);
-    b = fabs(x - y) * (pow(sin(z), 2) + tan(z));
-    s = a / b;
-    cout << "resultat s=" << s << endl;
-    return 0;
-}
+      using namespace std;
+
+      double x, y, z, s;
+      cout << "Vvedi x: ";
+      
+      cin >> x;
+      cout << "Vvedi y: ";
+      cin >> y;
+      cout << "Vvedi z: ";
+      cin >> z;
+      s = pow(2, -x) * sqrt(x + pow(fabs(y), 1 / 4.)) * pow(exp(x - 1 / sin(z)), 1 / 3.);
+      cout << "resultat s = " << s << endl;
+      return 0;
+}     
+
+ 
