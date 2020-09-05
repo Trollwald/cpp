52cf4a2e8c2890230a14e6d3fe52fbff0034f00b
John MG <johnmg@MacBook-Pro-John.local>
2020-09-05 09:13:06 +0300

    done laba

--- laba7.cpp
+++ laba7.cpp
@@ -4,11 +4,12 @@ int main()
 {
     using namespace std;
     string str, result = "";
+    cout << "Vvedite cifry: " << endl;
     getline(cin, str);
     for (int i = 0; i < str.size(); i++)
         if ((str[i] - '0') % 2 != 1)
             result += str[i];
-    cout << result << endl;
-    
+    cout << "Chetnie cifry:" << result << endl;
+
     return 0;
 }
