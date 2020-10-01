1a9bf0ec3667bb251e63ca4c703b6989f3a69fe7
John MG <johnmg@MacBook-Pro-John.local>
2020-10-01 09:25:43 +0300

    done

--- laba7.cpp
+++ laba7.cpp
@@ -9,6 +9,7 @@ int main()
     for (int i = 0; i < str.size(); i++)
         if (str[i]%2 == 0)
             result += str[i];
+            
     cout << "Chetnie cifry:" << result << endl;
 
     return 0;
