959de51d229e02896890b072c0fcc03d52736bcb
John MG <johnmg@MacBook-Pro-John.local>
2020-12-17 13:55:28 +0300

    laba 6 var 16

--- laba6.cpp
+++ laba6.cpp
@@ -6,39 +6,55 @@ using namespace std;
 int main()
 {
 
-    int n, i, j ,k;
+    int n, i, j, k, M, sum = 0, pr = 1;
     cout << "Vvedite kol-vo strok matrici: ";
     cin >> n;
-     cout << "Vvedite kol-vo stolbcov matrici: ";
+    cout << "Vvedite kol-vo stolbcov matrici: ";
     cin >> k;
-    cout << "Vvedite elementy matrici" << endl;
-    int** a = new int*[n];
+    // sled 3 stroki - din massiv
+    int **a = new int *[n];
     for (int i = 0; i < n; ++i)
-    a[i] = new int[k];
-
-    for (int i = 0; i < n; i++)
+        a[i] = new int[k];
+    cout << "1 or 2" << endl;
+    cin >> M;
+    if (M == 1)
     {
-
-        for (int j = 0; j < k; j++)
+        cout << "Vvedite elementy matrici" << endl;
+        for (int i = 0; i < n; i++)
         {
-            cin >> a[i][j];
+            for (int j = 0; j < k; j++)
+            {
+                cin >> a[i][j];
+            }
         }
     }
-    bool sim = true;
+    else
+    {
+        srand(time(NULL));
+        for (int i = 0; i < n; i++)
+        {
+
+            for (int j = 0; j < k; j++)
+            {
+                a[i][j] = rand() % 10 - 5;
+                cout << a[i][j] << endl;
+            }
+        }
+    }
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            if (a[i][j] != a[n - 1 - j][n - 1 - i])
-            {
-                sim = false;
-                break;
-            }
+            if (i > j)
+                sum += a[i][j];
+            else if (i < j)
+                pr *= a[i][j];
         }
-    
     }
-    cout << ((sim) ? "Matrica simmetrichna" : "Matrica ne simmetrichna") << endl;
-for (int i = 0; i < n; ++i)
-    delete [] a[i];
-delete [] a;
+    cout << "summa = " << sum << endl;
+    cout << "proizvedenie = " << pr << endl;
+    // ydalyaem znacheniya massiva
+    for (int i = 0; i < n; ++i)
+        delete[] a[i];
+    delete[] a;
 }
